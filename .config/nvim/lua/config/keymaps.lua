c35dab3e3f6949142ea5ce8595fe37c425619f7d
Aditya <adityasinghboss1234@gmail.com>
2024-01-18 04:57:38 +0530

    fix(nvim): correct keymaps for toggleInlayHints and rgbToHsl

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -52,5 +52,9 @@ keymap.set("n", "<C-j>", function()
 end, opts)
 
 keymap.set("n", "<leader>r", function()
-  require("craftzdog.utils").replaceHexWithHSL()
+  require("aditya.hsl").replaceHexWithHSL()
+end)
+
+keymap.set("n", "<leader>i", function()
+  require("aditya.lsp").toggleInlayHints()
 end)
